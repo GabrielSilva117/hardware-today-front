0748325c17c023b8b6967180421a9e2c57266ea7
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-09-15 16:42:29 -0300

    feat: Adds a NavBar component to the main layout

---
+++ src/components/MainNavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar
+    class="d-flex flex-column align-center justify-center"
+    scroll-behavior="collapse hide"
+  >
+    <v-toolbar-title>Main Navbar</v-toolbar-title>
+    <v-spacer />
+    <v-btn>Home</v-btn>
+    <v-btn>About</v-btn>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="sass">
+
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <v-app>
+    <MainNavBar />
+
     <v-main>
       <router-view />
     </v-main>
