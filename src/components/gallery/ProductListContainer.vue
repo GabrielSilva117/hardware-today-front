1ec87967d4dbac30aac582b6f1890a60a745e8a3
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-09-15 16:46:41 -0300

    feat: Adds the gallery product list container

---
+++ src/components/gallery/ProductListContainer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="products-list-container">
+    <v-btn class="btn btn-primary"> Sample Text </v-btn>
+    <p>tedfsadgsad</p>
+  </div>
+</template>
+
+<style scoped lang="css">
+  .products-list-container {
+    display: grid;
+    justify-items: center;
+    margin-top: 10px
+  }
+</style>
