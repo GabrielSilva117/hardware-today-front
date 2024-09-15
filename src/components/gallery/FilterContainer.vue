c7911fbfe4a50606f2757bc84179d6aa4b1e6a8f
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-09-15 16:46:24 -0300

    feat: Adds a gallery product filter container

---
+++ src/components/gallery/FilterContainer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-expansion-panels variant="inset">
+    <v-expansion-panel>
+      <v-expansion-panel-title expand-icon="">
+        <h3>Filters</h3>
+      </v-expansion-panel-title>
+
+      <v-expansion-panel-text>
+        <v-col cols="12">
+          <v-row>
+            <v-btn>Sample Text</v-btn>
+          </v-row>
+        </v-col>
+      </v-expansion-panel-text>
+
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<style scoped lang="sass">
+
+</style>
