e768166a2dd98e03167089514a4ace5f8ec81a8d
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-09-15 16:41:34 -0300

    feat: Adds a User Service class

---
+++ src/services/UserService.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import UserModel from '../models/UserModel'
+
+class UserService {
+  api
+
+  constructor () {
+    this.api = axios.create({
+      baseURL: 'http://localhost:8080/user',
+    })
+  }
+
+  post (data: UserModel, resource: string = '') {
+    // console.log(process.env.VUE_APP_API_URL)
+
+    return this.api.post(resource, data)
+  }
+
+  get (resource: string = '') {
+    return this.api.get(resource)
+  }
+}
+
+export default new UserService()
