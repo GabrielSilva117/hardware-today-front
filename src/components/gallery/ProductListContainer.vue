67a366dfb157fa2e4a7b375599b8b8f92e86ba26
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-09-17 19:03:23 -0300

    feat: Adds a listing logic to output every product on the component

--- src/components/gallery/ProductListContainer.vue
+++ src/components/gallery/ProductListContainer.vue
@@ -1,13 +1,27 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div class="products-list-container">
-    <v-btn class="btn btn-primary"> Sample Text </v-btn>
-    <p>tedfsadgsad</p>
-  </div>
+  <v-list lines="two">
+    <v-list-item v-for="product in products" :key="product.id" :subtitle="product.description" :title="product.name">
+      <p>Brand: {{ product.brand.name }}</p>
+      <p>Category: {{ product.category.name }}</p>
+      <p>R$ {{ product.price }}</p>
+    </v-list-item>
+  </v-list>
+<!--  <div  class="products-list-container">-->
+
+<!--  </div>-->
 </template>
+
+<script setup lang="ts">
+  import { ProductModel } from '@/models/product/ProductModel'
+  import ProductService from '@/services/ProductService'
+
+  const products: ProductModel[] = await getProducts()
+
+  async function getProducts () : Promise<ProductModel[]> {
+    const prodList = await ProductService.getProductList()
+    return prodList.data
+  }
+</script>
 
 <style scoped lang="css">
   .products-list-container {
