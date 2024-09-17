55f3d03c0dab68c531366fefe3fb788bc6d74cc5
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-09-17 19:01:12 -0300

    feat: Adds a get method to fetch all products

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,5 +1,17 @@
 import axios from 'axios'
 
-class ProductService {}
+class ProductService {
+  api
+
+  constructor () {
+    this.api = axios.create({
+      baseURL: 'http://localhost:8080/products',
+    })
+  }
+
+  getProductList () {
+    return this.api.get('')
+  }
+}
 
 export default new ProductService()
