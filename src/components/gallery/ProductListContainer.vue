7c0171d80811abe759581ac040ce9db9b19ed96c
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-11-18 18:51:54 -0300

    refactor: Changes the product list origin to a prop behavior

--- src/components/gallery/ProductListContainer.vue
+++ src/components/gallery/ProductListContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-list lines="two">
-    <v-list-item v-for="product in products" :key="product.id" :subtitle="product.description" :title="product.name">
+    <v-list-item v-for="product in props.products" :key="product.id" :subtitle="product.description" :title="product.name">
       <p>Brand: {{ product.brand.name }}</p>
       <p>Category: {{ product.category.name }}</p>
       <p>R$ {{ product.price }}</p>
@@ -12,15 +12,9 @@
 </template>
 
 <script setup lang="ts">
-  import { ProductModel } from '@/models/product/ProductModel'
-  import ProductService from '@/services/ProductService'
-
-  const products: ProductModel[] = await getProducts()
-
-  async function getProducts () : Promise<ProductModel[]> {
-    const prodList = await ProductService.getProductList()
-    return prodList.data
-  }
+  const props = defineProps({
+    products: { type: Array, required: true },
+  })
 </script>
 
 <style scoped lang="css">
