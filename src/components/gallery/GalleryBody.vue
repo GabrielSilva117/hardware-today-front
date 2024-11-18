3c2d5d75b472ca944c29de6d7762010eb7cb915f
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2024-11-18 18:52:59 -0300

    refactor: Adds the product list in the parent component
    WIP

--- src/components/gallery/GalleryBody.vue
+++ src/components/gallery/GalleryBody.vue
@@ -3,13 +3,26 @@
     class="align-centerfill-height mx-auto mt-3"
     max-width="900"
   >
-    <FilterContainer />
-    <ProductListContainer />
+    <FilterContainer :products="products" @filter-products="filterProducts" />
+    <ProductListContainer :products="products" />
   </v-responsive>
 </template>
 
 <script setup lang="ts">
+  import { ProductModel } from '@/models/product/ProductModel'
+  import ProductService from '@/services/ProductService'
 
+  const products: ProductModel[] = await getProducts()
+
+  function filterProducts (products: ProductModel[]) {
+    console.log(products)
+  }
+
+  async function getProducts () : Promise<ProductModel[]> {
+    const prodList = await ProductService.getProductList()
+    console.log(prodList.data)
+    return prodList.data
+  }
 </script>
 
 <style scoped lang="css">
